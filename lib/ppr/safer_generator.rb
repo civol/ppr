99b84a978dfedeaf40e6df0fe5cd0991698bd0b8
lovic <lovic@ariake-nct.ac.jp>
2022-11-13 11:00:23 +0900

    Removed usage of fork since it is not supported on Windows. Instead, use the same process but added the possibility to return back to unsafe mode.

--- lib/ppr/safer_generator.rb
+++ lib/ppr/safer_generator.rb
@@ -43,6 +43,16 @@ class SaferGenerator
         methods = DANGER_METHODS + @black_methods
         # Gather the constants to strip.
         constants = DANGER_CONSTANTS + @black_constants
+        # Save the dangerous methods in a private safe.
+        @safe_of_methods = {}
+        methods.each do |meth|
+            @safe_of_methods[meth]=method(meth)
+        end
+        # Save the dangerous constants in a private safe.
+        @safe_of_constants = {}
+        constants.each do |cst|
+            @safe_of_constants[cst] = Object.send(:const_get,cst)
+        end
         # Strip the dangerous methods.
         methods.each do |meth|
             Kernel.send(:undef_method,meth)
@@ -50,6 +60,21 @@ class SaferGenerator
         # Strip the dangerous constants from Object.
         constants.each do |cst|
             Object.send(:remove_const,cst)
+        end
+    end
+
+    # Restores all the stripped Kernel methods and constants appart from the
+    # elements of the white list.
+    # Also strip Object from dangerous methods and constants apart
+    # from the elements of the white list.
+    def unsecure
+        # Restores the dangerous methods in a private safe.
+        @safe_of_methods.each do |(name,pr)|
+            Kernel.send(:define_method,name,&pr)
+        end
+        # Restors the dangerous constants in a private safe.
+        @safe_of_constants.each do |(name,cst)|
+            Object.const_set(name,cst)
         end
     end
 
@@ -65,61 +90,97 @@ class SaferGenerator
         end
         # Creates the pipe for communicating with the block.
         rd,wr = IO.pipe
-        # Creates a process for executing the block.
-        pid = fork
-        if pid then
-            # This is the parent: waits for the block execution result.
-            # No need to write on the pipe. close it.
-            wr.close
-            # Read the result of the process and send it to stream
-            until rd.eof?
-                stream << rd.read
-            end
-            # No more need of rd.
-            rd.close
-            # Wait the end of the child process
-            Process.wait(pid)
-            # Where there a trouble?
-            unless $?.exited? then
-                # pid did not exit, internal error.
-                raise "*Internal error*: safer process #{pid} did not exit."
-            end
-            if $?.exitstatus !=0 then
-                # Reconstruct the exception from the stream, the exit
-                # status is the number of line to use.
-                e0 = Marshal.load( stream.string.each_line.
-                                   to_a[-$?.exitstatus..-1].join )
-                # Then resend the eception encapsulated into another one
-                # telling the safer process failed.
-                begin
-                    raise e0
-                rescue Exception => e1
-                    raise SaferException.new("*Error*: exception occured in safer process #{pid}.")
-                end
-            end
-        else
-            # This is the child: enter in safe mode and execute the block.
-            # No need to write on the pipe. close it.
-            rd.close
-            # Secure.
-            secure
-            # Execute the block.
-            begin
-                block.call(wr)
-            rescue Exception => e
-                # The exception is serialized and passed to the main process
-                # through the pipe.
-                e = Marshal.dump(e)
-                wr << "\n" << e 
-                # The exit status is the number of line of the serialized
-                # exception.
-                exit!(e.each_line.count)
-            end
-            # No more need of wr.
-            wr.close
-            # End the process without any error.
-            exit!(0)
+        # # Creates a process for executing the block.
+        # pid = fork
+        # if pid then
+        #     # This is the parent: waits for the block execution result.
+        #     # No need to write on the pipe. close it.
+        #     wr.close
+        #     # Read the result of the process and send it to stream
+        #     until rd.eof?
+        #         stream << rd.read
+        #     end
+        #     # No more need of rd.
+        #     rd.close
+        #     # Wait the end of the child process
+        #     Process.wait(pid)
+        #     # Where there a trouble?
+        #     unless $?.exited? then
+        #         # pid did not exit, internal error.
+        #         raise "*Internal error*: safer process #{pid} did not exit."
+        #     end
+        #     if $?.exitstatus !=0 then
+        #         # Reconstruct the exception from the stream, the exit
+        #         # status is the number of line to use.
+        #         e0 = Marshal.load( stream.string.each_line.
+        #                            to_a[-$?.exitstatus..-1].join )
+        #         # Then resend the eception encapsulated into another one
+        #         # telling the safer process failed.
+        #         begin
+        #             raise e0
+        #         rescue Exception => e1
+        #             raise SaferException.new("*Error*: exception occured in safer process #{pid}.")
+        #         end
+        #     end
+        # else
+        #     # This is the child: enter in safe mode and execute the block.
+        #     # No need to write on the pipe. close it.
+        #     rd.close
+        #     # Secure.
+        #     secure
+        #     # Execute the block.
+        #     begin
+        #         block.call(wr)
+        #     rescue Exception => e
+        #         # The exception is serialized and passed to the main process
+        #         # through the pipe.
+        #         e = Marshal.dump(e)
+        #         wr << "\n" << e 
+        #         # The exit status is the number of line of the serialized
+        #         # exception.
+        #         exit!(e.each_line.count)
+        #     end
+        #     # No more need of wr.
+        #     wr.close
+        #     # End the process without any error.
+        #     exit!(0)
+        # end
+        # 
+        # # Is there a string to return?
+        # if to_return then
+        #     return stream.string
+        # else
+        #     return nil
+        # end
+
+        # Secure.
+        secure
+        trouble = nil
+        # Execute the block.
+        begin
+            block.call(wr)
+        rescue Exception => e
+            trouble = e
         end
+        # No more need of wr.
+        wr.close
+
+        # Unsecure and process the result.
+        unsecure
+        # Read the result of the process and send it to stream
+        until rd.eof?
+            stream << rd.read
+        end
+        # No more need of rd.
+        rd.close
+        if trouble then
+            begin
+                raise trouble
+            rescue Exception => e1
+                raise SaferException.new("*Error*: exception occured in safe mode.")
+            end
+        end
+          
         # Is there a string to return?
         if to_return then
             return stream.string
